b51b518079068c57a989f7365aaea6d4ccda954f
Ethan Dye <mrtops03@gmail.com>
2025-04-16 14:29:38 -0600

    Try OSC 52 again
    Signed-off-by: Ethan Dye <mrtops03@gmail.com>

--- lua/options.lua
+++ lua/options.lua
@@ -20,6 +20,7 @@ vim.opt.showmode = false
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
 vim.schedule(function()
+  vim.g.clipboard = 'osc52'
   vim.opt.clipboard = 'unnamedplus'
 end)
 
