2615f4f6413324e18aa66599e940c1fe96b0cc7a
Ethan Dye <mrtops03@gmail.com>
2025-10-08 11:31:51 -0600

    Add keybinding to grug-far
    Signed-off-by: Ethan Dye <mrtops03@gmail.com>

--- lua/custom/plugins/grug-nvim.lua
+++ lua/custom/plugins/grug-nvim.lua
@@ -1,5 +1,14 @@
 return {
   'MagicDuck/grug-far.nvim',
   opts = {},
+  keys = {
+    {
+      '<leader>sp',
+      function()
+        require('grug-far').open()
+      end,
+      desc = '[S]earch & Re[p]lace (grug-far)',
+    },
+  },
 }
 -- vim: ts=2 sts=2 sw=2 et
